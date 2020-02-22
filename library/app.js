be6de426dd335249a3a54abdffb991a1a2f899cd
Bradly Goodner <bradlygoodner1442@gmail.com>
2020-02-22 12:20:59 -0600

    Created Router

--- library/app.js
+++ library/app.js
@@ -7,6 +7,7 @@ const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3000;
+const bookRouter = express.Router();
 
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public/')));
@@ -17,8 +18,27 @@ app.use('/js', express.static(path.join(__dirname, '/node_modules/@popperjs/dist
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
+bookRouter.route('/')
+  .get((req, res) => {
+    res.send('hello books');
+  });
+
+bookRouter.route('/single')
+  .get((req, res) => {
+  res.send('hello single book');
+});
+
+app.use('/books', bookRouter);
+
 app.get('/', (req, res) => {
-  res.render('index', { list: ['a', 'b', 'c'], title: 'Bradly Libray' });
+  res.render('index',
+    {
+      nav: [{ link: '/books', title: 'Books' },
+      { link: '/authors', title: 'Authors' }],
+      title: 'My Libray'
+
+    }
+  );
 });
 
 app.listen(port, () => {
